8a3be9a7aca18ee5cf4d053fa0054ad93ede07b2
Yared Getachew <veduket@gmail.com>
2017-07-20 18:20:33 +0300

    Modified a get_data.php to return a better error

--- libs/get_data.php
+++ libs/get_data.php
@@ -1,5 +1,13 @@
 <?php ini_set('output_buffering', 'on');
 require_once "helper.inc.php";
 header("Content-Type: application/json;encoding:utf-8");
-echo json_encode(get_cbe_data()['data']);
+$json = json_encode(get_cbe_data()['data']);
+if($json===false){
+  $json = json_encode(array("JSON_ERROR",json_last_error_msg()));
+  if($json===false){
+    $json='{"JSON_ERROR":"Unknown JSON Error"}';
+  }
+  http_response_code(500);
+}
+echo $json;
 ?>
